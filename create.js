b0eb0b893d788f8fb1debe835f3285dd2bd74def
RajatSharma17 <52863211+RajatSharma17@users.noreply.github.com>
2020-02-24 11:55:00 -0500

    Test

--- create.js
+++ create.js
@@ -3,31 +3,33 @@ const connect = require('./db');
 const Voter = require('./schema');
 
 connect();
-
+const fs = require('fs');
+const readline = readline.createInterface({
+  input: fs.createReadStream('voters.csv');
+})
 const arr = [];
-const createdvalue = [];
-//this was done using 1
-const rows = Voter.split('\n');
-const data = rows.map(b => b.split(','));
 
-for(const rows of data)
-{
+file.on('line', function(line){
+  const data = line.split(',');
+  arr.push(new Voter({
+  'firstname': data[0];
+  'lastname': data[1];
+  'zipcode': data[2];
+  'history': data[3];
+}));
+});
 
-    createdvalue = new Voter({
-      'firstname': data[0];
-      'lastname': data[1];
-      'zipcode': data[2];
-      'history': data[3];
-    })
-    arr.push(createdvalue[]);
 
-}
+
+
 
 
 
 // Reset the data
+file.on('close', function() {
 mongoose.connection.dropDatabase()
-  .then(() => arr.save())
+  .then(() => Promise.all(arr.map(d => d.save()))
   .then(() => mongoose.connection.close())
   .then(() => console.log('Database is ready.'))
   .catch(error => console.error(error.stack));
+});
